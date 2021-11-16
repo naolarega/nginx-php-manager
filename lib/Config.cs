07d1b18560ab5a14bdf1354dd6a0215a60f5f67e
naolArega <na408498@gmail.com>
2021-11-16 16:51:11 +0300

    added config util singltone to read configurations

---
+++ lib/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace nginx_php_manager.lib
+{
+    public class Config
+    {
+        public enum ConfigStatus
+        {
+            SUCCESS,
+            FAILED,
+            NO_CONFIG
+        };
+        private static string configFileName = "config";
+        private static XmlDocument configDocumment;
+
+        private Config() { }
+
+        public static ConfigStatus read()
+        {
+            if (File.Exists(configFileName))
+            {
+                configDocumment = new XmlDocument();
+                try
+                {
+                    configDocumment.Load(
+                        string.Format("{0}\\{1}.xml", Directory.GetCurrentDirectory(), configFileName)
+                        );
+                    return ConfigStatus.SUCCESS;
+
+                }
+                catch
+                {
+                    return ConfigStatus.FAILED;
+                }
+            }
+            else
+            {
+                return ConfigStatus.NO_CONFIG;
+            }
+        }
+    }
+}
